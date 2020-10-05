a351b19223ac30cc773721adee8483f48ca92021
Adam Mrowicki <adam.mrowicki@atena.pl>
2020-10-05 22:15:14 +0200

    Importing GUI

--- NewEnvironment/NewEnvironment.psm1
+++ NewEnvironment/NewEnvironment.psm1
@@ -1,7 +1,8 @@
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Functions\Public\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Functions\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$GUI = @( Get-ChildItem -Path $PSScriptRoot\GUI\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 
-foreach ($Import in @($Public + $Private))
+foreach ($Import in @($Public + $Private + $GUI))
 {
     try { . $Import.FullName }
     catch { Write-Error -Message "Failed to Import function $($Import.fullname): $_" }
