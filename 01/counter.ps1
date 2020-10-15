597925ac6d1c732022913e17a2c146fdf460dcd7
Adam Mrowicki <adam.mrowicki@atena.pl>
2020-10-15 11:17:56 +0200

    Counter

---
+++ 01/counter.ps1
@@ -0,0 +1,18 @@
+$script:count = 0
+$script:increment = 1
+
+function Get-Count
+{
+    return $script:count += $script:increment
+}
+
+function Reset-Count
+{
+    $script:count = 0
+    setIncrement 1
+}
+
+function setIncrement ($x)
+{
+    $script:increment = $x
+}
